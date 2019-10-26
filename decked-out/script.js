574c13acd620548caf547289e73097a83bdf2df5
vipulpandey12345 <54221581+vipulpandey12345@users.noreply.github.com>
2019-10-25 22:11:23 -0400

    Update script.js

--- decked-out/script.js
+++ decked-out/script.js
@@ -1,5 +1,19 @@
-if (name==null || name=="")
-    {
-    alert("First name must be filled out");
-    return false;
-    }
+var button = document.querySelector("/submit")
+var inputValue = document.querySelector(".inputType")
+var name = document.querySelector("name")
+
+
+.button.addEventListener('click', function(){
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&
+,appid="81422bbb32515a09b5683222c3eb76b2")
+    .then(response => response.json())
+    .then(data => {
+        var nameValue = data['name'];
+        var temp = data['main']['temp'];
+
+        name.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+
+    })
+.catch(err => ("Please enter a valid city name"))
+})
