43a97a07f64b10c7a664aec2d9e31ebc7487ff25
vipulpandey12345 <54221581+vipulpandey12345@users.noreply.github.com>
2019-12-26 00:37:24 -0800

    Update script.js

--- decked-out/script.js
+++ decked-out/script.js
@@ -1,5 +1,27 @@
-if (name==null || name=="")
-    {
-    alert("First name must be filled out");
-    return false;
-    }
+$(document).ready(function(){
+
+    $('#submitButton').on('click', function(){
+
+        console.log("button was clicked");
+        var city = $("#city").val();
+
+        if(city != " "){
+            $.ajax({
+                url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + 
+                "&APPID=0f811edb1ef54f07062c6e3574da2120", api.openweathermap.org/data/2.5/weather?q=London
+                type: "GET",
+                dataType: "jsonp",
+                success: function(data){
+                    console.log(data);
+                }
+            });
+
+        }else{
+            $("#error").html("Please submit a proper city name")
+
+        }
+
+    });
+
+});
+
